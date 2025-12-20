813ab561e5f4c1d1f2cc82d38d8d820bffac08c3
deni <deni.nes3364@gmail.com>
2025-12-20 22:54:22 +0200

    Add Firebase config and Vercel configuration for deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2IslvkWwdui2qwiXdp5U1eRaNAQlKSuY",
+  authDomain: "deni-erty.firebaseapp.com",
+  projectId: "deni-erty",
+  storageBucket: "deni-erty.firebasestorage.app",
+  messagingSenderId: "61059861990",
+  appId: "1:61059861990:web:6c3d502aaecbe1f40c5a27",
+  measurementId: "G-TJVS4LYBF2"
+};
